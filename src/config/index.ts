8d66fded249770be2a15a4109101b0312a50330c
AlexCPT-cpu <alexodey79@gmail.com>
2025-03-12 21:37:19 +0100

    init

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const headerColor = "#324B51";
+export const navBarColor = "#456770";
+
+export const status = {
+  active: "#1FB544",
+  pending: "#EB9F13",
+  stop: "#E64141",
+};
+
+export const card = {
+  background: "#F3F3F3",
+  border: "#E8E8E8",
+};
